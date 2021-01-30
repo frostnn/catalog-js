4c162fa75062c9a430ad5f40e6cd384bae03a572
frostnn <gradysnn92@yandex.ru>
2021-01-30 16:29:41 +0300

    add styles for cards

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,16 +1,24 @@
 class Products {
   render() {
     let htmlCatalog = '';
-    catalog.forEach(({ id, title, img, price }) => {
+    catalog.forEach(({ id, title, img, price, currency = dollar }) => {
       htmlCatalog += `
-        <div class="col-4">
+        <div class="col-4 mb-5">
           <div class="catalog-item">
             <div class="catalog-item_img">
-            <img src="${img}" alt="">   
+              <img src="${img}" alt="">   
             </div>
-            <div class="catalog-item_title">${title}</div>
-            <div class="catalog-item_price">${price}</div>
-            <button lass="catalog-item_btn">Add to cart</button>
+            <div class="catalog-item_title">
+              ${title}
+            </div>
+            <div class="catalog-item_wrapper-price">
+              <span class="catalog-item_price">
+                ${price} ${currency}
+              </span>
+              <button class="catalog-item_btn btn draw-border">
+                Add to cart
+              </button>
+            </div>
           </div>
         </div>
       `;
